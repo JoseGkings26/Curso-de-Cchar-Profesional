55176a18de17a8d8da96928d9e6db24ab9ca86e0
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2022-09-20 23:57:41 -0500

    Ejercicio7_extra terminado

--- Estructura_Condicional/Ejercicio7_extra.cs
+++ Estructura_Condicional/Ejercicio7_extra.cs
@@ -10,8 +10,20 @@ namespace Estructura_Condicional
     {
         public void determinarNumeroTresDigitos()
         {
-         //   Hacer un algoritmo que lea un número por el teclado y determine si tiene tres dígito
-       
+            //   Hacer un algoritmo que lea un número por el teclado y determine si tiene tres dígito
+            int num;
+            Console.WriteLine("Ingrese un Número: ");
+            num = int.Parse(Console.ReadLine());
+            
+            if (num>=100 && num<=999)
+            {
+                Console.WriteLine("Tiene 3 dígitos");
+            }
+            else
+            {
+                Console.WriteLine("No tiene 3 dígitos");
+            }
+            Console.ReadKey();
         }
     }
 }
